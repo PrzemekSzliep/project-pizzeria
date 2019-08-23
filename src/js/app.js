d3d4a76b0ac34e66eb9e6e01f17faf27c1b1b68b
Przemek Szliep <szlipek@gmail.com>
2019-08-23 18:55:36 +0200

    Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {Product} from './components/Product.js';
 import {Cart} from './components/Cart.js';
-import {select, settings} from './settings.js';
+import {select, settings, classNames} from './settings.js';
+import {Booking} from './components/Booking.js';
 
 const app = {
   initData: function () {
@@ -51,6 +52,59 @@ const app = {
     });
   },
 
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+    let pagesMatchingHash = [];
+
+    if (window.location.hash.length > 2) {
+
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      pagesMatchingHash = thisApp.pages.filter(function(page) {
+        return page.id == idFromHash;
+      });
+      thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+    }
+
+    for (let link of thisApp.navLinks) {
+
+      link.addEventListener('click', function(event) {
+
+        const clickedElement = this;
+        event.preventDefault();
+
+        const idPage = clickedElement.getAttribute('href');
+
+        const link = idPage.replace('#', '');
+
+        thisApp.activatePage(link);
+      });
+    }
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#'+ pageId);
+    }
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.nav.active, page.getAttribute('id') == pageId);
+    }
+
+    window.location.hash = '#/' + pageId;
+  },
+
+  initBooking: function() {
+    const thisApp = this;
+
+    thisApp.bookingContainer = document.querySelector(select.containerOf.booking);
+
+    new Booking(thisApp.bookingContainer);
+  },
+
   init: function () {
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -59,9 +113,14 @@ const app = {
     // console.log('settings:', settings);
     // console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
+
   },
+
+
 };
 
 app.init();
