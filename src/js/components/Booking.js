3d27b4798da4ccecdbf21cc345148310fa95cb31
Przemek Szliep <szlipek@gmail.com>
2019-09-01 12:23:07 +0200

    fix

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {templates, select} from '../settings.js';
 import {utils} from '../utils.js';
 import {AmountWidget} from './AmountWidget.js';
+import {DatePicker} from './DatePicker.js';
+import {HourPicker} from './HourPicker.js';
 
 export class Booking {
   constructor(element) {
@@ -19,6 +21,8 @@ export class Booking {
     thisBooking.html = utils.createDOMFromHTML(generatedHTML);
     thisBooking.dom.peopleAmount = thisBooking.html.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.html.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.html.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.html.querySelector(select.widgets.hourPicker.wrapper);
 
     thisBooking.dom.wrapper.appendChild(thisBooking.html);
   }
@@ -27,5 +31,7 @@ export class Booking {
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
---
+++ src/js/components/HourPicker.js
@@ -0,0 +1,44 @@
+/* global rangeSlider */
+
+import {BaseWidget} from './BaseWidget.js';
+import {select, settings} from '../settings.js';
+import {utils} from '../utils.js';
+
+export class HourPicker extends BaseWidget {
+  constructor(wrapper) {
+    super(wrapper, settings.hours.open);
+
+    const thisWidget = this;
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapper;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+
+    thisWidget.initPlugin(thisWidget.value);
+  }
+
+  initPlugin() {
+    const thisWidget = this;
+
+    rangeSlider.create(thisWidget.dom.input, {
+      onSlide: function(value) {
+        thisWidget.value = value;
+      },
+    });
+
+  }
+
+  parseValue(newValue) {
+    return utils.numberToHour(newValue);
+  }
+
+  isValid() {
+    return true;
+  }
+
+  renderValue() {
+    const thisWidget = this;
+    thisWidget.dom.output.innerHTML = thisWidget.value;
+  }
+}
